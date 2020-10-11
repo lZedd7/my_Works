4835011f8364a5231711ed0262b5d06e66566163
lZedd7 <alexsydorenko7r@gmail.com>
2020-10-11 23:51:02 +0300

    закончил с адаптивкой
    решить проблему с логотипом

--- maket_1--mavic/app/js/main.js
+++ maket_1--mavic/app/js/main.js
@@ -18,6 +18,17 @@ $(function(){
         scrollHorizontally: true,
         sectionSelector: '.page-section',
         scrollOverflow:true,
-	});
+        anchors: ['top', 'products', 'benefits', 'specification', 'questions','contacts'],
+	    menu: '#header__nav',
+    });
+    $('.menu__btn').on('click', function(){
+        $('.menu__btn').toggleClass('menu__btn--active');
+        $('.menu__list').toggleClass('menu__list--active');
+    });
+
+    $('.menu__list-link').on('click', function(){
+        $('.menu__btn').removeClass('menu__btn--active');
+        $('.menu__list').removeClass('menu__list--active');
+    });
   
 });
